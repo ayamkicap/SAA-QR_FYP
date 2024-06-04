88e3e8d6562b7924edea846b65e0009b08db75bd
Asri Comel <asrihafiz00@gmail.com>
2024-06-05 03:10:43 +0800

    update log handler

--- bsaaqr/middleware/errorHandler.js
+++ bsaaqr/middleware/errorHandler.js
@@ -1,6 +1,8 @@
 const { logEvents } = require('./logger')
+const connectDB = require('../config/dbConn');
+const mongoose = require('mongoose');
 
-const errorHandler = (err, req, res, next) => {
+const errorHandler = async (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
     console.log(err.stack)
 
@@ -9,6 +11,26 @@ const errorHandler = (err, req, res, next) => {
     res.status(status)
 
     res.json({ message: err.message })
-}
+
+    try {
+        const db = await connectDB();
+        const collection = db.collection('errorLogs');
+        
+        const log = {
+            name: err.name,
+            message: err.message,
+            method: req.method,
+            url: req.url,
+            origin: req.headers.origin,
+            stack: err.stack,
+            timestamp: new Date()
+        };
+
+        await collection.insertOne(log);
+        console.log('Error logged to MongoDB');
+    } catch (dbError) {
+        console.error('Failed to log error to MongoDB', dbError);
+    }
+};
 
 module.exports = errorHandler 
--- bsaaqr/middleware/logger.js
+++ bsaaqr/middleware/logger.js
@@ -3,6 +3,8 @@ const { v4: uuid } = require('uuid')
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
+const connectDB = require('../config/dbConn');
+const mongoose = require('mongoose');
 
 const logEvents = async (message, logFileName) => {
     const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
@@ -13,15 +15,34 @@ const logEvents = async (message, logFileName) => {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
         }
         await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+
+        // Log to MongoDB
+        const db = mongoose.connection;
+        const logCollection = db.collection('logs');
+
+        const log = {
+            dateTime,
+            id: uuid(),
+            message,
+            logFileName,
+            timestamp: new Date()
+        };
+
+        await logCollection.insertOne(log);
+        console.log('Log event recorded in MongoDB');
     } catch (err) {
         console.log(err)
     }
+
+    
 }
 
 const logger = (req, res, next) => {
     logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
     console.log(`${req.method} ${req.path}`)
     next()
+
+    
 }
 
 module.exports = { logEvents, logger }
