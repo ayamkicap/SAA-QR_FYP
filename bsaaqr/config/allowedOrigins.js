552519246ffffd8a7f8dd1bda4717834f9d0ec8f
Asri Comel <asrihafiz00@gmail.com>
2024-06-13 05:22:32 +0800

    add cors

--- bsaaqr/config/allowedOrigins.js
+++ bsaaqr/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3001',
     'https://www.dandrepairshop.com',
     'https://dandrepairshop.com',
-    'https://saa-qr.vercel.app:10000'
+    'https://saa-qr.vercel.app'
 ]
 
 module.exports = allowedOrigins
